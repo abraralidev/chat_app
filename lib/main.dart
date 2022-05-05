958609089ed6e0d29df0eff5e59b7577b7f93627
Abrar Ali <abraraliiofficial@gmail.com>
2022-05-05 18:57:04 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const ChatApp());
+
+class ChatApp extends StatelessWidget {
+  const ChatApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: const WelcomeScreen(),
+    );
+  }
+}
